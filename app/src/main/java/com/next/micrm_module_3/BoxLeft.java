f21b65184842b7bbf2fabd87102d44731f8f119f
Walter Alejandro Camaly <waltercamaly@gmail.com>
2016-06-08 15:30:10 -0300

    Initial commit

---
+++ app/src/main/java/com/next/micrm_module_3/BoxLeft.java
@@ -0,0 +1,12 @@
+package com.next.micrm_module_3;
+
+import android.annotation.TargetApi;
+import android.app.Fragment;
+import android.os.Build;
+
+/**
+ * Created by wcamaly on 07/06/2016.
+ */
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class BoxLeft extends Fragment {
+}
